5ce9458dff55d1a5de4659041aa04f3b9b13ecb5
David Yamnitsky <davidy@me.com>
2015-01-04 15:40:53 -0800

    fix bugs in setup

--- setup.sh
+++ setup.sh
@@ -18,7 +18,8 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git .zprezto
 echo "installing zprezto... done"
 
 echo "symlinking dotfiles..."
-files = ".aliases .functions .gitconfig .vim .vimrc .zlogin .zprezto .zpreztorc .zshrc"
+mkdir -p .vim/bundle
+files=".aliases .functions .gitconfig .vim .vimrc .zlogin .zprezto .zpreztorc .zshrc"
 for file in $files; do
   rm ~/$file
   echo "creating symlink for $file"
@@ -27,12 +28,10 @@ done
 echo "symlinking dotfiles... done"
 
 echo "installing vim packages..."
-mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh
-vim +PluginInstall +qall
 echo "installing vim packages... done"
 
 echo "*** all done, close this terminal window and open a new one ***"
